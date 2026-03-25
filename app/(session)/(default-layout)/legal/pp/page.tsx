ea1ac8935aa4e550c523a841d0cf3749753bb103
Lulu <lutfilkhaliq587@gmail.com>
2026-03-25 08:35:16 +0800

    add content for /legal/pp

--- app/(session)/(default-layout)/legal/pp/page.tsx
+++ app/(session)/(default-layout)/legal/pp/page.tsx
@@ -1,3 +1,7 @@
 export default function PP() {
-    return <>Privacy Policy</>
+    return <div>
+        <h1>Privacy Policy</h1>
+        <h2>How we use your Google data</h2>
+        <p>Keeping it simple without all the legal talk: We only keep your Email address and pair it with a uses count. Thats basically it</p>
+    </div>
 }
